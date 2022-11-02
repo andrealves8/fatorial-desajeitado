7d6a26732999230b705743384bf254b91025bbc6
André Alves <73435241+andrealves8@users.noreply.github.com>
2022-11-02 11:36:44 -0300

    refatorando

--- src/Main.java
+++ src/Main.java
@@ -5,85 +5,72 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in); 
+  
+        int n = Integer.parseInt(new Scanner(System.in).nextLine());
+        int primeiroDaFila;
+        int segundoDaFila;
+        int calculo = 0;
+        int posMultDiv = 0;
+        int posSoma = 0;
+        int etapa = 1;
+        int res = 0;
+        int[] listaOperacoes = new int[n];
 
-    int N = scanner.nextInt();
-    int primeiroDaFila; 
-    int segundoDaFila;
-    int calculo = 0;
-    int posMultDiv = 0;
-    int posSoma = 0;
-    int etapa = 1;
-    int resultado = 0;
-    int[] listaOperacoes = new int[N];
-   
-    List<Integer> lista = new LinkedList<>();
-    // Guarda os números de N a 1;
-    for (int index = N; index > 0; index--) {
-        lista.add(index); 
-    }
+        List<Integer> lista = new LinkedList<>();
+        
+        for (int index = n; index > 0; index--) {
+            lista.add(index);
+        }
+        
+        while (!lista.isEmpty() && lista.size() > 1) {
+            primeiroDaFila = lista.size();
+            segundoDaFila = lista.size() - 1;
 
-    // Calcula por etapas e guarda em um array
-    while (!lista.isEmpty() && lista.size() > 1) {
-        primeiroDaFila = lista.size();         
-        segundoDaFila = lista.size() - 1;
- 
-        if(primeiroDaFila == lista.size() && etapa == 1) {
-            listaOperacoes[posMultDiv] = primeiroDaFila * segundoDaFila;
-            calculo = listaOperacoes[posMultDiv];
-            lista.remove(0);             
-            etapa = 2;   
+            if (primeiroDaFila == lista.size() && etapa == 1) {
+                listaOperacoes[posMultDiv] = primeiroDaFila * segundoDaFila;
+                calculo = listaOperacoes[posMultDiv];
+                lista.remove(0);
+                etapa = 2;
 
-        } else 
+            } else if (primeiroDaFila == lista.size() && etapa == 2) {
+                listaOperacoes[posMultDiv] = calculo / segundoDaFila;
+                calculo = listaOperacoes[posMultDiv];
+                lista.remove(0);
+                etapa = 3;
 
-        if(primeiroDaFila == lista.size() && etapa == 2) {
-            listaOperacoes[posMultDiv] = calculo / segundoDaFila;
-            calculo = listaOperacoes[posMultDiv];             
-            lista.remove(0);             
-            etapa = 3;
-            
-        } else
+            } else if (primeiroDaFila == lista.size() && etapa == 3) {
+                calculo = calculo + segundoDaFila;
+                listaOperacoes[posMultDiv + 1] = segundoDaFila;
+                lista.remove(0);
+                etapa = 4;
+                posMultDiv++;
 
-        if(primeiroDaFila == lista.size() && etapa == 3) {
-            calculo = calculo + segundoDaFila;
-            listaOperacoes[posMultDiv + 1] = segundoDaFila;
-            lista.remove(0);             
-            etapa = 4;            
-            posMultDiv++;
-            
-        } else
+            } else if (primeiroDaFila == lista.size() && etapa == 4) {
+                lista.remove(0);
+                posMultDiv++;
+                posSoma++;
+                etapa = 1;
+            }
 
-        if(primeiroDaFila == lista.size() && etapa == 4 && lista.iterator().hasNext()) {
-            lista.remove(0);            
-            posMultDiv++;            
-            posSoma++;            
-            etapa = 1;
-                      
         }
 
-        } // Fim do While
-        
-        resultado = listaOperacoes[0];
+        res = listaOperacoes[0];
 
-        for (int i = 0; i < listaOperacoes.length -1; i++) {
-            
-            if(listaOperacoes[i + 1] != 0) {
-                resultado += listaOperacoes[i + 1];
+        for (int i = 0; i < listaOperacoes.length - 1; i++) {
+
+            if (listaOperacoes[i + 1] != 0) {
+                res += listaOperacoes[i + 1];
                 i++;
             }
-            
-            if(listaOperacoes[i + 1] != 0) {
-                resultado -= listaOperacoes[i + 1];
+
+            if (listaOperacoes[i + 1] != 0) {
+                res -= listaOperacoes[i + 1];
             }
 
         }
-        // Resultado Final
-        System.out.println(resultado);
-   
+       
+        System.out.println(res);
+
     }
 
-        @Override
-        public String toString() {
-        return "Definitivo []";
-        }
 }
